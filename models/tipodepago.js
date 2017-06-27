bac2a7a405e801c5523fd38e5761cecdd4b17c58
felixfermin7 <felixfermin7@gmail.com>
2017-06-26 21:02:37 -0400

    3

--- models/tipodepago.js
+++ models/tipodepago.js
@@ -12,7 +12,7 @@ return db.query("Select * from tipo_pago",callback);
 return db.query("select * from tipo_pago where Id=?",[id],callback);
  },
  addtipopago:function(tipopago,callback){
- return db.query("Insert into tipo_pago values(?)",[tipopago.descripcion],callback);
+ return db.query("Insert into tipo_pago (descripcion) values(?)",[tipopago.descripcion],callback);
  },
  deletetipopago:function(id,callback){
   return db.query("delete from tipo_pago where Id=?",[id],callback);
