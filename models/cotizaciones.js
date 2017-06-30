531866ae48e31c3350669a3841a2dd0e048b3df7
felixfermin7 <felixfermin7@gmail.com>
2017-06-29 22:39:17 -0400

    1706292

--- models/cotizaciones.js
+++ models/cotizaciones.js
@@ -21,5 +21,6 @@ return db.query("select * from cotizacion where codigo=?",[id],callback);
   return db.query("update cotizacion set fecha=?,cliente=?, dir_cliente=?, lugar_entrega=?, descuento=?, incremento=?, flete=?, tipo_pago=?, nota=?, vendedor=?, iva=? where codigo=?",[cotizacion.fecha, cotizacion.cliente, cotizacion.dir_cliente, cotizacion.lugar_entrega, cotizacion.descuento, cotizacion.incremento, cotizacion.flete, cotizacion.tipo_pago, cotizacion.nota, cotizacion.vendedor, cotizacion.iva ,id],callback);
  }
  
+ 
 };
  module.exports=cotizacion;
